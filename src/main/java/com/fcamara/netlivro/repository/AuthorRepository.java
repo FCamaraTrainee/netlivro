4b439e48277536bb119af402907bf033e94f84bc
Walter dos Santos Simoni <waltersantossimoni@gmail.com>
2023-02-14 12:02:07 -0300

    feat: new method find with active

--- src/main/java/com/fcamara/netlivro/repository/AuthorRepository.java
+++ src/main/java/com/fcamara/netlivro/repository/AuthorRepository.java
@@ -1,12 +1,13 @@
 package com.fcamara.netlivro.repository;
 
 import com.fcamara.netlivro.model.Author;
-import com.fcamara.netlivro.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+  Optional<Author> findByIdAndActiveFalse(UUID uuid);
 }
