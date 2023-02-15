d23a10986e1fbafa991377e93fff2ef1a052b37d
Lupi <lupi.bruno@outlook.com>
2023-02-15 11:23:48 -0300

    feat: adiciona services de Book

--- src/main/java/com/fcamara/netlivro/service/BookService.java
+++ src/main/java/com/fcamara/netlivro/service/BookService.java
@@ -1,5 +1,18 @@
 package com.fcamara.netlivro.service;
 
-public interface BookService {
+import com.fcamara.netlivro.model.Author;
+import com.fcamara.netlivro.model.Book;
+import com.fcamara.netlivro.model.Category;
 
+import java.util.List;
+import java.util.UUID;
+
+public interface BookService {
+    Book findBookById(UUID id);
+    List<Book> findByAuthor(Author author);
+    List<Book> findByCategory (Category category);
+    List<Book> findByYearRange (Integer startYear, Integer endYear);
+    Book createBook(Book book);
+    void deleteBook(UUID id);
+    List<Book> findAll();
 }
