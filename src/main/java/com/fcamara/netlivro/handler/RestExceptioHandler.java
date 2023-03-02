05bdc1b2f4e94238e542839f860d1ad44101d337
Marcelo Henrique Ribeiro <mghenr@gmail.com>
2023-03-02 10:42:18 -0300

    hotfix exception handler

--- src/main/java/com/fcamara/netlivro/handler/RestExceptioHandler.java
+++ src/main/java/com/fcamara/netlivro/handler/RestExceptioHandler.java
@@ -1,6 +1,8 @@
 package com.fcamara.netlivro.handler;
 
 import com.fcamara.netlivro.exception.IllegalAuthorException;
+import com.fcamara.netlivro.exception.IllegalBookException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +14,11 @@ public class RestExceptioHandler {
         String message = ex.getMessage();
         System.out.println("Deu erro aqui gente!!!");
         return new ResponseEntity<>(message, ex.getStatusCode());
+    }
+    @ExceptionHandler(IllegalBookException.class)
+    public static ResponseEntity<String> IllegalArgumentExceptionHandler(IllegalBookException ex) {
+        String message = ex.getMessage();
+        System.out.println("Deu erro aqui gente!!!");
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
